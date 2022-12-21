c3209a8233b5deccc0632888d21886f1670367da
Leejoonkwon <107663853+Leejoonkwon@users.noreply.github.com>
2022-12-21 16:14:15 +0900

    Bert upload 2022-12-21

---
+++ Bert/bert_try.py
@@ -0,0 +1,503 @@
+#==============================================================
+# 1. 환경설정
+
+# import tensorflow as tf
+import torch
+
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+from transformers import get_linear_schedule_with_warmup
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+# from keras.preprocessing.sequence import pad_sequences
+from keras_preprocessing.sequence import pad_sequences
+# from pad_sequences import pad_sequences_multi
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+import random
+import time
+import datetime
+
+# GPU
+n_devices = torch.cuda.device_count()
+print(n_devices)
+
+for i in range(n_devices):
+    print(torch.cuda.get_device_name(i))
+    
+    
+#==============================================================    
+# 2. 데이터셋 로드
+
+import csv
+import pandas as pd
+
+# 11,876개의 한글 대화 문답 데이터(0,1,2 3가지 분류로 라벨링)
+chatbot_data = pd.read_csv('D:/ChatbotData.csv', encoding="utf-8")
+print(chatbot_data.shape) # (11823, 3)
+print(chatbot_data.head())
+#                  Q            A  label
+# 0           12시 땡!   하루가 또 가네요.      0
+# 1      1지망 학교 떨어졌어    위로해 드립니다.      0
+# 2     3박4일 놀러가고 싶다  여행은 언제나 좋죠.      0
+# 3  3박4일 정도 놀러가고 싶다  여행은 언제나 좋죠.      0
+# 4          PPL 심하네   눈살이 찌푸려지죠.      0
+
+chatbot_data.loc[(chatbot_data['label'] == 2), 'label'] = 1  # 라벨 1과 2는 모두 1로 통일시켜 이진분류로 변경
+chatbot_data_shuffled = chatbot_data.sample(frac=1).reset_index(drop=True)
+# train data & test data
+train = chatbot_data_shuffled[:9000]
+test = chatbot_data_shuffled[9000:]
+
+print(train.shape) # (9000, 3)
+print(test.shape)  # (2823, 3)
+
+
+#==============================================================
+# 3. train set 전처리
+
+# CLS, SEP 붙이기 (문장의 시작, 끝)
+sentences = ["[CLS] " + str(s) + " [SEP]" for s in train.Q]
+sentences[:5]
+labels = train['label'].values
+print('label',labels.shape)
+import pandas as pd
+from transformers import BertTokenizer
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased", do_lower_case=False)
+result = tokenizer.tokenize('안녕하세요!')
+print(result)
+tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
+tokenized_texts = [tokenizer.tokenize(s) for s in sentences]
+print(sentences[0])  # 토크나이징 전
+print(tokenized_texts[0]) # 토크나이징 후
+MAX_LEN = 128 # 최대 시퀀스 길이 설정
+input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+input_ids = pad_sequences(input_ids, padding='post',dtype='long',
+                                truncating='post', maxlen=MAX_LEN, )
+
+
+
+
+# def pad_sequences_multi(sequences: List[List[List[Number]]],
+#                         padding: Optional[str] = 'pre',
+#                         truncating: Optional[str] = 'pre',
+#                         maxlen: Optional[int] = None,
+#                         value: Optional[List[Number]] = None
+#                         ) -> List[List[List[Number]]]:
+
+# tf keras pad_sequences 사용 시
+# input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+attention_masks = []
+
+for seq in input_ids:
+    seq_mask = [float(i>0) for i in seq]
+    attention_masks.append(seq_mask)
+print(attention_masks[0])
+train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids,
+                                                                                    labels, 
+                                                                                    random_state=2000, 
+                                                                                    test_size=0.1)
+train_masks, validation_masks, _, _ = train_test_split(attention_masks, 
+                                                       input_ids,
+                                                       random_state=2000, 
+                                                       test_size=0.1)
+train_inputs = torch.tensor(train_inputs)
+train_labels = torch.tensor(train_labels)
+train_masks = torch.tensor(train_masks)
+validation_inputs = torch.tensor(validation_inputs)
+validation_labels = torch.tensor(validation_labels)
+validation_masks = torch.tensor(validation_masks)
+batch_size = 32
+
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
+
+
+#==============================================================
+# 4. test set 전처리
+
+# [CLS] + 문장 + [SEP]
+sentences = ["[CLS] " + str(sentence) + " [SEP]" for sentence in test.Q]
+
+# 라벨 데이터
+labels = test['label'].values
+print('test_label',labels.shape)
+# Word 토크나이저 토큰화
+tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
+tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
+
+# 시퀀스 설정 및 정수 인덱스 변환 & 패딩
+MAX_LEN = 128
+input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+input_ids = pad_sequences(input_ids, padding='post',
+                                truncating='post', maxlen=MAX_LEN, dtype="long")
+
+# tf keras pad_sequences 사용 시
+# input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+# 어텐션 마스크
+attention_masks = []
+for seq in input_ids:
+    seq_mask = [float(i>0) for i in seq]
+    attention_masks.append(seq_mask)
+    
+# 파이토치 텐서로 변환
+test_inputs = torch.tensor(input_ids)
+test_labels = torch.tensor(labels)
+test_masks = torch.tensor(attention_masks)
+print(validation_inputs.shape)
+print(validation_masks.shape)
+print(validation_labels.shape)
+# test
+# torch.Size([9000, 128])
+# torch.Size([9000, 128])
+# torch.Size([2823])
+# train
+# torch.Size([8100, 128])
+# torch.Size([8100, 128])
+# torch.Size([8100]
+# valid
+# torch.Size([900, 128])
+# torch.Size([900, 128])
+# torch.Size([900])
+# 배치 사이즈 설정 및 데이터 설정
+batch_size = 32
+test_data = TensorDataset(test_inputs, test_masks, test_labels)
+test_sampler = RandomSampler(test_data)
+test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
+
+
+#==============================================================
+# 5. 모델 생성
+
+if torch.cuda.is_available():    
+    device = torch.device("cuda")
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print('No GPU available, using the CPU instead.')
+model = BertForSequenceClassification.from_pretrained("bert-base-multilingual-cased", num_labels=2)
+model.cuda()
+# 옵티마이저
+optimizer = AdamW(model.parameters(),
+                  lr = 2e-5, # 학습률(learning rate)
+                  eps = 1e-8 
+                )
+
+# 에폭수
+epochs = 20
+
+# 총 훈련 스텝 : 배치반복 횟수 * 에폭
+total_steps = len(train_dataloader) * epochs
+
+# 스케줄러 생성
+scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                            num_warmup_steps = 0,
+                                            num_training_steps = total_steps)
+
+
+#==============================================================
+# 6. 모델 학습
+
+# 정확도 계산 함수
+def flat_accuracy(preds, labels):
+    
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+    
+    
+# 시간 표시 함수
+def format_time(elapsed):
+
+    # 반올림
+    elapsed_rounded = int(round((elapsed)))
+    
+    # hh:mm:ss으로 형태 변경
+    return str(datetime.timedelta(seconds=elapsed_rounded))
+#랜덤시드 고정
+seed_val = 42
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+
+#그래디언트 초기화
+model.zero_grad()
+
+# 학습
+for epoch_i in range(0, epochs):
+    
+    # ========================================
+    #               Training
+    # ========================================
+    
+    print("")
+    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+    print('Training...')
+
+    # 시작 시간 설정
+    t0 = time.time()
+
+    # 로스 초기화
+    total_loss = 0
+
+    # 훈련모드로 변경
+    model.train()
+        
+    # 데이터로더에서 배치만큼 반복하여 가져옴
+    for step, batch in enumerate(train_dataloader):
+        # 경과 정보 표시
+        if step % 500 == 0 and not step == 0:
+            elapsed = format_time(time.time() - t0)
+            print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
+
+        # 배치를 GPU에 넣음
+        batch = tuple(t.to(device) for t in batch)
+        
+        # 배치에서 데이터 추출
+        b_input_ids, b_input_mask, b_labels = batch
+
+        # Forward 수행                
+        outputs = model(b_input_ids, 
+                        token_type_ids=None, 
+                        attention_mask=b_input_mask, 
+                        labels=b_labels)
+        
+        # 로스 구함
+        loss = outputs[0]
+
+        # 총 로스 계산
+        total_loss += loss.item()
+
+        # Backward 수행으로 그래디언트 계산
+        loss.backward()
+
+        # 그래디언트 클리핑
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        # 그래디언트를 통해 가중치 파라미터 업데이트
+        optimizer.step()
+
+        # 스케줄러로 학습률 감소
+        scheduler.step()
+
+        # 그래디언트 초기화
+        model.zero_grad()
+
+    # 평균 로스 계산
+    avg_train_loss = total_loss / len(train_dataloader)            
+
+    print("")
+    print("  Average training loss: {0:.2f}".format(avg_train_loss))
+    print("  Training epcoh took: {:}".format(format_time(time.time() - t0)))
+        
+    # ========================================
+    #               Validation
+    # ========================================
+
+    print("")
+    print("Running Validation...")
+
+    #시작 시간 설정
+    t0 = time.time()
+
+    # 평가모드로 변경
+    model.eval()
+
+    # 변수 초기화
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+
+    # 데이터로더에서 배치만큼 반복하여 가져옴
+    for batch in validation_dataloader:
+        # 배치를 GPU에 넣음
+        batch = tuple(t.to(device) for t in batch)
+        
+        # 배치에서 데이터 추출
+        b_input_ids, b_input_mask, b_labels = batch
+        
+        # 그래디언트 계산 안함
+        with torch.no_grad():     
+            # Forward 수행
+            outputs = model(b_input_ids, 
+                            token_type_ids=None, 
+                            attention_mask=b_input_mask)
+        
+        # 로스 구함
+        logits = outputs[0]
+
+        # CPU로 데이터 이동
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+        
+        # 출력 로짓과 라벨을 비교하여 정확도 계산
+        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+        eval_accuracy += tmp_eval_accuracy
+        nb_eval_steps += 1
+
+    print("  Accuracy: {0:.2f}".format(eval_accuracy/nb_eval_steps))
+    print("  Validation took: {:}".format(format_time(time.time() - t0)))
+
+print("")
+print("Training complete!")
+
+
+
+#==============================================================
+# 7. test set 평가
+
+#시작 시간 설정
+t0 = time.time()
+
+# 평가모드로 변경
+model.eval()
+
+# 변수 초기화
+eval_loss, eval_accuracy = 0, 0
+nb_eval_steps, nb_eval_examples = 0, 0
+
+# 데이터로더에서 배치만큼 반복하여 가져옴
+for step, batch in enumerate(test_dataloader):
+    # 경과 정보 표시
+    if step % 100 == 0 and not step == 0:
+        elapsed = format_time(time.time() - t0)
+        print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(test_dataloader), elapsed))
+
+    # 배치를 GPU에 넣음
+    batch = tuple(t.to(device) for t in batch)
+    
+    # 배치에서 데이터 추출
+    b_input_ids, b_input_mask, b_labels = batch
+    
+    # 그래디언트 계산 안함
+    with torch.no_grad():     
+        # Forward 수행
+        outputs = model(b_input_ids, 
+                        token_type_ids=None, 
+                        attention_mask=b_input_mask)
+    
+    # 로스 구함
+    logits = outputs[0]
+
+    # CPU로 데이터 이동
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+    
+    # 출력 로짓과 라벨을 비교하여 정확도 계산
+    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+    eval_accuracy += tmp_eval_accuracy
+    nb_eval_steps += 1
+
+print("")
+print("Accuracy: {0:.2f}".format(eval_accuracy/nb_eval_steps))
+print("Test took: {:}".format(format_time(time.time() - t0)))
+
+
+#==============================================================
+# 8. 새로운 문장 테스트
+
+# 입력 데이터 변환
+def convert_input_data(sentences):
+
+    # BERT의 토크나이저로 문장을 토큰으로 분리
+    tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
+
+    # 입력 토큰의 최대 시퀀스 길이
+    MAX_LEN = 128
+
+    # 토큰을 숫자 인덱스로 변환
+    input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+    
+    # 문장을 MAX_LEN 길이에 맞게 자르고, 모자란 부분을 패딩 0으로 채움
+    input_ids = pad_sequences(input_ids, padding='post', 
+                                    truncating='post', maxlen=MAX_LEN, dtype="long")
+
+    # tf keras pad_sequences 사용 시
+    # input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+    
+    # 어텐션 마스크 초기화
+    attention_masks = []
+
+    # 어텐션 마스크를 패딩이 아니면 1, 패딩이면 0으로 설정
+    # 패딩 부분은 BERT 모델에서 어텐션을 수행하지 않아 속도 향상
+    for seq in input_ids:
+        seq_mask = [float(i>0) for i in seq]
+        attention_masks.append(seq_mask)
+
+    # 데이터를 파이토치의 텐서로 변환
+    inputs = torch.tensor(input_ids)
+    masks = torch.tensor(attention_masks)
+
+    return inputs, masks
+# 문장 테스트
+def test_sentences(sentences):
+
+    # 평가모드로 변경
+    model.eval()
+
+    # 문장을 입력 데이터로 변환
+    inputs, masks = convert_input_data(sentences)
+
+    # 데이터를 GPU에 넣음
+    b_input_ids = inputs.to(device)
+    b_input_mask = masks.to(device)
+            
+    # 그래디언트 계산 안함
+    with torch.no_grad():     
+        # Forward 수행
+        outputs = model(b_input_ids, 
+                        token_type_ids=None, 
+                        attention_mask=b_input_mask)
+
+    # 로스 구함
+    logits = outputs[0]
+
+    # CPU로 데이터 이동
+    logits = logits.detach().cpu().numpy()
+
+    return logits
+logits = test_sentences(['더 나은 학교생활 하고 싶어'])
+print(logits)
+
+if np.argmax(logits) == 1 :
+    print("연애 관련 대화")
+elif np.argmax(logits) == 0 :
+    print("일상 대화")
+logits = test_sentences(['저녁 뭘 먹을지 추천해줘'])
+
+print(logits)
+if np.argmax(logits) == 1 :
+    print("연애 관련 대화")
+elif np.argmax(logits) == 0 :
+    print("일상 대화")
+    
+logits = test_sentences(['지원 누나가 키보드를 쾅쾅 내리쳤다'])
+
+print(logits)
+if np.argmax(logits) == 1 :
+    print("분노")
+elif np.argmax(logits) == 0 :
+    print("장난")
+    
+   
+   
+   
+   
+ 
+    
+    
+    
+    
+    
+    
